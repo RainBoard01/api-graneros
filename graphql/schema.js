c9b5f113e55e15f282c54ef8678073b8a9a3be18
RainBoard01 <amdmsi4ever@hotmail.com>
2019-09-11 21:34:58 -0300

    Added all Delete Mutations

--- graphql/schema.js
+++ graphql/schema.js
@@ -316,6 +316,72 @@ const schema = new GraphQLSchema({
                         useFindAndModify: false
                     });
                 }
+            },
+            deleteRecord: {
+                type: RecordType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Record.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
+            },
+            deletePerson: {
+                type: PersonType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Person.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
+            },
+            deleteVehicle: {
+                type: VehicleType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Vehicle.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
+            },
+            deleteParcel: {
+                type: ParcelType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Parcel.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
+            },
+            deleteOwner: {
+                type: OwnerType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Owner.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
+            },
+            deleteGuard: {
+                type: GuardType,
+                args: {
+                    id: { type: GraphQLNonNull(GraphQLID) }
+                },
+                async resolve(parent, args) {
+                    return await Guard.findByIdAndRemove(args.id,{
+                        useFindAndModify: false
+                    });
+                }
             }
         }
     })
