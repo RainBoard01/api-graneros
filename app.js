e3b537957689b16784e509f5bbd4b8ec2c78c7c9
RainBoard01 <amdmsi4ever@hotmail.com>
2019-09-15 21:24:45 -0300

    Added cors dependency

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors =require('@koa/cors');
 const { execute, errorHandler } = require('graphql-api-koa');
 const schema = require('./graphql/schema');
 const dbInit = require('./database');
 
 dbInit();
 const app = new Koa()
+    .use(cors())
     .use(errorHandler())
     .use(bodyParser())
     .use(execute({ schema }))
